c968729905b75bfc92aba3079668e30cfef9de45
Jaldo <rom1.jaldo@gmail.com>
2019-03-20 22:57:42 +0100

    background impg + fix sqlite3 in gemfile + begin config locales + fix devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'home#index'
-  resources :home, only: [:index]
+    mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+    devise_for :users
 
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    root to: 'home#index'
+    resources :home, only: [:index]
   # get 'home', to: 'home#index'
 end
