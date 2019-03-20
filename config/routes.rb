2706764471aec88f78cbb71fdfbc3d92f1183019
Jaldo <rom1.jaldo@gmail.com>
2019-03-20 23:57:06 +0100

    add route to listings + update front homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-    mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-    devise_for :users
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  devise_for :users
 
-    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-    root to: 'home#index'
-    resources :home, only: [:index]
-  # get 'home', to: 'home#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'home#index'
+  resources :home, only: [:index]
+  #get 'home', to: 'home#index'
+
+  root to: 'listings#index'
+  resources :listing, only: [:index]  
+
 end
